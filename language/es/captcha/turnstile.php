dd483347d2c933caf166510f8f1281ea1839c168
Alfredo Ramos <alfredo.ramos.sanchez@gmail.com>
2026-02-06 18:31:29 -0600

    New translations turnstile.php (Spanish)

--- language/es/captcha/turnstile.php
+++ language/es/captcha/turnstile.php
@@ -49,8 +49,5 @@ $lang = array_merge($lang, [
 	'TURNSTILE_NOSCRIPT' => 'Por favor, habilite JavaScript en su navegador web para cargar el desafío.',
 	'TURNSTILE_LOGIN_ERROR_ATTEMPTS' => 'Ha superado el número máximo de intentos de inicio de sesión permitidos.<br>Además de su nombre de usuario y contraseña, se utilizará Turnstile para autenticar su sesión.',
 
-	'CLOUDFLARE_REQUEST_EXCEPTION' => 'Error de solicitud de Cloudflare: %s',
-
-	'ACP_CLOUDFLARE_TOGGLE_SECRET' => 'Alternar visibilidad de %s',
-	'ACP_CLOUDFLARE_VALIDATE_INVALID_FIELDS' => 'Valores inválidos para los campos: <samp>%s</samp>'
+	'CLOUDFLARE_REQUEST_EXCEPTION' => 'Error de solicitud de Cloudflare: %s'
 ]);
