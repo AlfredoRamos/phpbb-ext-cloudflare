ac8b3f12968173a06a0c4b659455d400106e9b93
Alfredo Ramos <alfredo.ramos.sanchez@gmail.com>
2026-02-06 18:31:41 -0600

    New translations controller.php (Spanish, Mexico)

--- language/es_x_tu/controller.php
+++ language/es_x_tu/controller.php
@@ -25,5 +25,9 @@ $lang = array_merge($lang, [
 	'EXCEPTION_CLOUDFLARE_NO_API_DATA' => 'Cloudflare API key and Zone ID are mandatory.',
 	'EXCEPTION_CLOUDFLARE_AJAX_ONLY' => 'This route can only be used on AJAX calls.',
 
-	'CLOUDFLARE_ERR_PURGE_CACHE_TYPE' => 'Invalid purge cache type.'
+	'CLOUDFLARE_ERR_PURGE_CACHE_TYPE' => 'Invalid purge cache type.',
+	'CLOUDFLARE_ERR_RULESET_TYPE' => 'Invalid ruleset type.',
+	'CLOUDFLARE_ERR_RULESET_UPDATE' => 'Could not update ruleset.',
+	'CLOUDFLARE_ERR_RULESET_RULES_LIST' => 'Invalid ruleset rules.',
+	'CLOUDFLARE_ERR_RULESET_RULES_UPDATE' => 'Could not update ruleset rules.'
 ]);
